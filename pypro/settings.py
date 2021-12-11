602be58228d988a28049da6311600782f7e9269d
moderno <tiago_figueira@hotmail.com>
2021-12-11 00:23:05 -0300

    Atualização

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sie(o5n1r01fsjv9#devrkg$e%(88409zw1q8i5p=9!aol=o7d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
