46f2f0956f0097d1bcbe946e9ed750b4c8433b86
moderno <tiago_figueira@hotmail.com>
2021-12-22 19:03:25 -0300

    Criação de diretórios pra receber arquivos estáticos e de upload de arquivos pro django1.

--- pypro/settings.py
+++ pypro/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')  #
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
