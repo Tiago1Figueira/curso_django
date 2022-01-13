8b9318ecd06c257e7fba60d897a03a491690457c
moderno <tiago_figueira@hotmail.com>
2022-01-12 22:29:13 -0300

    Atualização dos códigos dos arquivos para habilitar o comando ' mng showmigrations'.

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,7 +32,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-auth_user_models = 'base.User'
+AUTH_USER_MODEL = 'base.User'
 
 # Application definition
 
