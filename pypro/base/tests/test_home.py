5c9227457103b57f982f77e2cc7be45be09c6c62
moderno <tiago_figueira@hotmail.com>
2022-01-25 22:33:18 -0300

    Atualização Pipfile.lock1.

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -7,6 +7,7 @@ def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
 
+
 def test_title(client: Client):
     resp = client.get('/')
     assert_contains(resp, '<title>Python Pro</title>')
