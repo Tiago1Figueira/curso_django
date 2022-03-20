bce1fcc4cd7ac18f54e1fddc1638ae5197d7e123
moderno <tiago_figueira@hotmail.com>
2022-03-20 20:48:19 -0300

    Utilizar filtro no titulo do video.  close #250

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def video(request, slug):
     videos = {
         'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 682327604},
-        'instalacao_windows': {'titulo': 'Instalação Windows', 'vimeo_id': 690291392},
+        'instalacao_windows': {'titulo': '', 'vimeo_id': 690291392},
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
