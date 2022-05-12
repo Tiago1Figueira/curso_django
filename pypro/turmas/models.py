e50860aada4aa9c8102dce65bafc35219033d54d
moderno <tiago_figueira@hotmail.com>
2022-05-12 14:59:40 -0300

    Campo ManyToManyField          close #580

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,4 +1,4 @@
-# from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -7,3 +7,4 @@ class Turma(models.Model):
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
+    matriculas = models.ManyToManyField(get_user_model())
