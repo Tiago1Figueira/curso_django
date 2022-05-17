aa7d59b2f32b30b19c89c0e854449afe05593c63
moderno <tiago_figueira@hotmail.com>
2022-05-17 14:07:13 -0300

    Teste com Usuário Logado       close #629

--- pypro/django_assertions.py
+++ pypro/django_assertions.py
@@ -3,3 +3,4 @@ from django.test import TestCase
 _test_case = TestCase()
 
 assert_contains = _test_case.assertContains  # método não está seguindo a PEP8. Deveria ser snakecase.
+assert_not_contains = _test_case.assertNotContains
